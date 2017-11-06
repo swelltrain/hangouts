7ae6180520880d4701601b552c8e1eb2b647861d
Stephen Philp <stephen@shelf.com>
2017-11-06 11:39:39 -0800

    Hangouts cleanup
      remove vestigial counter variable

--- src/hangouts.cr
+++ src/hangouts.cr
@@ -2,7 +2,6 @@ require "./hangouts/*"
 
 module Hangouts
   conversations = Reader.new("/Users/stephen/crystal/hangouts/data/hangouts.json").call
-  counter = 0
   melanie = Participant.new("103495081214667156826", "id", "Melanie Barnett", "phone")
   conversations.each do |conversation|
     if conversation.has_participant?(melanie)
