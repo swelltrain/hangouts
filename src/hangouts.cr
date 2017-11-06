3bdb64d5de59b697998a32afeb946fbb016601fd
Stephen Philp <stephen@shelf.com>
2017-11-06 11:38:25 -0800

    Hangouts refactor
      move the creation of the conversation outside the loop

--- src/hangouts.cr
+++ src/hangouts.cr
@@ -3,8 +3,8 @@ require "./hangouts/*"
 module Hangouts
   conversations = Reader.new("/Users/stephen/crystal/hangouts/data/hangouts.json").call
   counter = 0
+  melanie = Participant.new("103495081214667156826", "id", "Melanie Barnett", "phone")
   conversations.each do |conversation|
-    melanie = Participant.new("103495081214667156826", "id", "Melanie Barnett", "phone")
     if conversation.has_participant?(melanie)
       conversation.sorted_events.each { |event| puts event }
     end
